0cb985cbd6be042597bd5c954c820c2af1a37d1e
thuan <thuan135765@gmail.com>
2023-05-18 10:08:33 +0700

    create entity promotion

---
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/entity/Promotion.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gangoffive.birdtradingplatform.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.Date;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Admins
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Promotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+//    @ManyToMany
+//    @JoinTable(
+//        name = "ProductPromotion",
+//        joinColumns = @JoinColumn(name = "promotionId"),
+//        inverseJoinColumns = @JoinColumn(name = "productId")
+//    )
+//    private List<String> courses;
+  
+    
+    @Column
+    private String description;
+    
+    @Column
+    private int discountRate;
+    
+    @Column
+    private Date starDate;
+    
+    @Column
+    private Date endDate;
+}
