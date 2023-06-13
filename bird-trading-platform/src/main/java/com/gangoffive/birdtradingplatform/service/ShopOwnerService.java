58dfb0a6cf16045997553fde6a4efc7512aba942
tien <hoangtienbmt2911@gmail.com>
2023-06-13 19:53:45 +0700

    Do bar-chart data

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/service/ShopOwnerService.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/service/ShopOwnerService.java
@@ -1,14 +1,24 @@
 package com.gangoffive.birdtradingplatform.service;
 
+import com.gangoffive.birdtradingplatform.dto.BarChartDto;
+import com.gangoffive.birdtradingplatform.dto.BarChartOneTypeDto;
 import com.gangoffive.birdtradingplatform.dto.LineChartDto;
 import com.gangoffive.birdtradingplatform.dto.PieChartDto;
 import com.gangoffive.birdtradingplatform.entity.Account;
+import com.gangoffive.birdtradingplatform.entity.Order;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface ShopOwnerService {
     List<LineChartDto> getDataLineChart(String email, Date dateFrom);
+
     List<PieChartDto> getDataPieChart(String email);
-    double dataPieChartByTypeProduct(Account account, Class<?> productClass);
+    List<BarChartDto> dataBarChartByPriceAllTypeProduct(String email);
+    List<Order> getAllOrdersPreviousWeek(Account account);
+    List<LocalDate> getAllDatePreviousWeek();
+    List<BarChartDto> dataBarChartByOrderAllTypeProduct(String email);
+    List<BarChartOneTypeDto> dataBarChartByPriceEachTypeProduct(
+            Account account, Class<?> productClass, boolean isCalcPrice, boolean isCalcQuantity);
 }
