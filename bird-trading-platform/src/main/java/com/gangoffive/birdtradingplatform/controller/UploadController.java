4fde9462b0964fb392f6acb725d1194936509e64
tien <hoangtienbmt2911@gmail.com>
2023-06-15 16:26:28 +0700

    Authentication, Upload file

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/UploadController.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/UploadController.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/api/v1/upload")
 @Slf4j
 public class UploadController {
     @GetMapping
