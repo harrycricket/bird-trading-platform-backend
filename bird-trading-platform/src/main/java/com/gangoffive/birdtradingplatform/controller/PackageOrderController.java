7c399913a2d67a1ff070c76fa6e65e706a4c8b1f
tien <hoangtienbmt2911@gmail.com>
2023-06-23 23:16:38 +0700

    Fix UserInfo

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/PackageOrderController.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/PackageOrderController.java
@@ -1,11 +1,18 @@
 package com.gangoffive.birdtradingplatform.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gangoffive.birdtradingplatform.dto.PackageOrderRequestDto;
 import com.gangoffive.birdtradingplatform.service.PackageOrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
