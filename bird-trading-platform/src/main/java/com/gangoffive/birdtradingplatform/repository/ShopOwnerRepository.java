10f6d3a740fef9ac5f3c25b96ce2ea4982df9987
thong <vanthong07012002@gmail.com>
2023-06-24 11:42:57 +0700

    create api for get shop infor base on user id

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/repository/ShopOwnerRepository.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/repository/ShopOwnerRepository.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import com.gangoffive.birdtradingplatform.entity.ShopOwner;
 
+import java.util.Optional;
+
 @Repository
 public interface ShopOwnerRepository extends JpaRepository<ShopOwner, Long>{
+
+    Optional<ShopOwner> findByAccount_Id(long id);
 
 }
