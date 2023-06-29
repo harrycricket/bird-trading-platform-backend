6b2c4c48b9447f238c0d1fbecd76dee46db574f6
thuan <thuan135765@gmail.com>
2023-06-29 22:47:42 +0700

    get list staff

---
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/dto/ShopStaffDto.java
@@ -0,0 +1,16 @@
+package com.gangoffive.birdtradingplatform.dto;
+
+import com.gangoffive.birdtradingplatform.enums.AccountStatus;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class ShopStaffDto {
+    private Long id;
+    private String userName;
+    private AccountStatus status;
+}
