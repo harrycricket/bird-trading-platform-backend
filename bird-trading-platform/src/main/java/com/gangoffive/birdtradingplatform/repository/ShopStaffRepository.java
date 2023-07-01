42d5529407b9a6789b5eab0c4dedefaff4b6ddff
thong <vanthong07012002@gmail.com>
2023-07-01 17:49:09 +0700

    create api for update status code of shop staff

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/repository/ShopStaffRepository.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/repository/ShopStaffRepository.java
@@ -2,9 +2,13 @@ package com.gangoffive.birdtradingplatform.repository;
 
 import com.gangoffive.birdtradingplatform.dto.ShopStaffDto;
 import com.gangoffive.birdtradingplatform.entity.ShopOwner;
+import com.gangoffive.birdtradingplatform.enums.AccountStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gangoffive.birdtradingplatform.entity.ShopStaff;
@@ -16,4 +20,9 @@ import java.util.Optional;
 public interface ShopStaffRepository extends JpaRepository<ShopStaff, Long>{
     Optional<ShopStaff> findByUserName(String userName);
     Page<ShopStaff> findByShopOwner(ShopOwner shopOwner, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value = "Update tblShop_Staff_Acc s Set s.status = ?1 where s.id In ?2 And s.shopOwner.id = ?3")
+    int updateStatusWithShopId(AccountStatus accountStatus, List<Long> staffId, long shopId);
 }
