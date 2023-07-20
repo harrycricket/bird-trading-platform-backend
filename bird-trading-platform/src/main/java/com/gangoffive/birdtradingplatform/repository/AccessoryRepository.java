05596fcca5f93c5a3b6376f10af4d682821527eb
thong <vanthong07012002@gmail.com>
2023-07-21 01:10:40 +0700

    fix load product detail for shop

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/repository/AccessoryRepository.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/repository/AccessoryRepository.java
@@ -116,7 +116,7 @@ public interface AccessoryRepository extends JpaRepository<Accessory, Long> {
                                                                       Pageable pageable);
 
     Optional<List<Accessory>> findAllByShopOwnerAndStatus(ShopOwner shopOwner, ProductStatus productStatus);
-    
+
     Optional<List<Accessory>> findByTagsInAndShopOwner_IdAndStatusIn(List<Tag> tag, long shopId, List<ProductStatus> productStatuses);
 
     Optional<List<Accessory>> findByTagsInAndShopOwner_IdAndStatus(List<Tag> tags, long shopId, ProductStatus productStatus);
