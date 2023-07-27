3100e349742d1911a48e79d68a890403f8de4367
tien <hoangtienbmt2911@gmail.com>
2023-07-28 01:35:40 +0700

    fix load promotion

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/PromotionController.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/PromotionController.java
@@ -3,6 +3,8 @@ package com.gangoffive.birdtradingplatform.controller;
 import com.gangoffive.birdtradingplatform.dto.PromotionDto;
 import com.gangoffive.birdtradingplatform.dto.PromotionFilterDto;
 import com.gangoffive.birdtradingplatform.service.PromotionService;
+import com.gangoffive.birdtradingplatform.util.JsonUtil;
+import com.gangoffive.birdtradingplatform.util.ResponseUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,19 +25,12 @@ public class PromotionController {
         return promotionService.createPromotion(promotionDto);
     }
 
-//    @GetMapping("/admin/promotions")
-//    public ResponseEntity<?> filterAllPromotions(@RequestParam String data) {
-//        try {
-//            return promotionService.filterAllPromotion(JsonUtil.INSTANCE.getObject(data, PromotionFilterDto.class));
-//        } catch (Exception e) {
-//            return ResponseUtils.getErrorResponseBadRequest("Data parse not correct.");
-//        }
-//    }
-
-    @PostMapping("/promotions")
-    public ResponseEntity<?> filterAllPromotions(@RequestBody PromotionFilterDto promotionFilter) {
-
-        return promotionService.filterAllPromotion(promotionFilter);
-
+    @GetMapping("/admin/promotions")
+    public ResponseEntity<?> filterAllPromotions(@RequestParam String data) {
+        try {
+            return promotionService.filterAllPromotion(JsonUtil.INSTANCE.getObject(data, PromotionFilterDto.class));
+        } catch (Exception e) {
+            return ResponseUtils.getErrorResponseBadRequest("Data parse not correct.");
+        }
     }
 }
