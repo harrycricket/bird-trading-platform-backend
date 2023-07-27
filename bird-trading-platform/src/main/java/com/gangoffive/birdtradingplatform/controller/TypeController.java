29f608b656d383bcd9ca486ff5d76a7ea24e06f0
thong <vanthong07012002@gmail.com>
2023-07-27 21:23:28 +0700

    add create type for admin platform

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/TypeController.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/TypeController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,4 +53,8 @@ public class TypeController {
         return ResponseEntity.ok(typeAccessories);
     }
 
+    @PostMapping("types/bird")
+    public ResponseEntity<?> createNewBirdType() {
+//        return typeBirdService.createNewAccessories();
+    }
 }
