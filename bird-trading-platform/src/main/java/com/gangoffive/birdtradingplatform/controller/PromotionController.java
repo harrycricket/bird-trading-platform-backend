16abf23914e1e43aa86d916ec911148db6bacea6
tien <hoangtienbmt2911@gmail.com>
2023-07-28 01:33:30 +0700

    fix load promotion

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/PromotionController.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/controller/PromotionController.java
@@ -2,10 +2,7 @@ package com.gangoffive.birdtradingplatform.controller;
 
 import com.gangoffive.birdtradingplatform.dto.PromotionDto;
 import com.gangoffive.birdtradingplatform.dto.PromotionFilterDto;
-import com.gangoffive.birdtradingplatform.dto.ReviewShopOwnerFilterDto;
 import com.gangoffive.birdtradingplatform.service.PromotionService;
-import com.gangoffive.birdtradingplatform.util.JsonUtil;
-import com.gangoffive.birdtradingplatform.util.ResponseUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,21 +12,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class PromotionController {
     private final PromotionService promotionService;
+
     @GetMapping("promotions")
     public ResponseEntity<?> getAllPromotion() {
         return promotionService.getAllPromotion();
     }
+
     @PostMapping("admin/promotion")
     public ResponseEntity<?> createPromotion(@RequestBody PromotionDto promotionDto) {
         return promotionService.createPromotion(promotionDto);
     }
 
-    @GetMapping("/admin/promotions")
-    public ResponseEntity<?> filterAllPromotions(@RequestParam String data) {
-        try {
-            return promotionService.filterAllPromotion(JsonUtil.INSTANCE.getObject(data, PromotionFilterDto.class));
-        } catch (Exception e) {
-            return ResponseUtils.getErrorResponseBadRequest("Data parse not correct.");
-        }
+//    @GetMapping("/admin/promotions")
+//    public ResponseEntity<?> filterAllPromotions(@RequestParam String data) {
+//        try {
+//            return promotionService.filterAllPromotion(JsonUtil.INSTANCE.getObject(data, PromotionFilterDto.class));
+//        } catch (Exception e) {
+//            return ResponseUtils.getErrorResponseBadRequest("Data parse not correct.");
+//        }
+//    }
+
+    @PostMapping("/promotions")
+    public ResponseEntity<?> filterAllPromotions(@RequestBody PromotionFilterDto promotionFilter) {
+
+        return promotionService.filterAllPromotion(promotionFilter);
+
     }
 }
