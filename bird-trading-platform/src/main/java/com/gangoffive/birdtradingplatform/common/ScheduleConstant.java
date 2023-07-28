46c41d38bf2d20ba7ce25264ae33286cdd6fb5c0
thong <vanthong07012002@gmail.com>
2023-07-28 13:45:12 +0700

    fix time cron

--- bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/common/ScheduleConstant.java
+++ bird-trading-platform/src/main/java/com/gangoffive/birdtradingplatform/common/ScheduleConstant.java
@@ -1,5 +1,5 @@
 package com.gangoffive.birdtradingplatform.common;
 
 public class ScheduleConstant {
-    public static final long TIME_BEFORE_TAKE_THE_VOUCHER = 12 * 60 * 60 * 1000;
+    public static final long TIME_BEFORE_TAKE_THE_VOUCHER = 6 * 60 * 60 * 1000;
 }
